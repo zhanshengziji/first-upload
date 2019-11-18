a1da2b59ca7f63445def92dad9e94d643e5f447d
zhanshengziji <Administrator@DESKTOP-6KV8DLU>
2019-11-18 10:39:18 +0800

    第一次上传项目到GitHub

---
+++ src/main/java/a/test/TestUserDao.java
@@ -0,0 +1,25 @@
+package a.test;
+
+import org.junit.Test;
+
+import a.dao.UserDao;
+import a.entity.User;
+
+public class TestUserDao {
+
+	@Test
+	public void test1() {
+		UserDao dao = new UserDao();
+		dao.addUser1();
+	}
+	
+	@Test
+	public void test2() {
+		UserDao dao = new UserDao();
+		
+		User user = new User(7, "rose", "rose123", 77.77);
+		
+		dao.addUser2(user);
+	}
+	
+}
